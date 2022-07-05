ad3de5f7fa8b662fa3da8bf0845e21722f61e16a
sagarchandresha <sagar.bsquaresoft@gmail.com>
2022-07-05 11:20:27 +0530

    changes in product page

--- src/components/ProductDetails.client.jsx
+++ src/components/ProductDetails.client.jsx
@@ -13,7 +13,7 @@ export default function ProductDetails({ product }) {
         <div className="grid items-start gap-6 lg:gap-20 md:grid-cols-2 lg:grid-cols-3">
           <div className="grid md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 md:w-full lg:col-span-2">
             <div className="md:col-span-2 snap-center card-image aspect-square md:w-full w-[80vw] shadow rounded">
-              {/* <ProductGallery media={product.media.nodes} /> */}
+              <ProductGallery media={product.media.nodes} />
             </div>
           </div>
           <div className="sticky md:mx-auto max-w-xl md:max-w-[24rem] grid gap-8 p-0 md:p-6 md:px-0 top-[6rem] lg:top-[8rem] xl:top-[10rem]">
@@ -25,7 +25,7 @@ export default function ProductDetails({ product }) {
                 {product.vendor}
               </span>
             </div>
-            {/* <ProductForm product={product} /> */}
+            <ProductForm product={product} />
             <div className="mt-8">
               <div
                 className="prose border-t border-gray-200 pt-6 text-black text-md"
@@ -39,146 +39,146 @@ export default function ProductDetails({ product }) {
   );
 }
 
-// function ProductForm({ product }) {
-//   const { options, selectedVariant } = useProductOptions();
+function ProductForm({ product }) {
+  const { options, selectedVariant } = useProductOptions();
+ console.log(options);
+  const isOutOfStock = !selectedVariant?.availableForSale || false;
+  return (
+    <form className="grid gap-10">
+      {
+        <div className="grid gap-4">
+          {options.map(({ name, values }) => {
+            if (values.length === 1) {
+              return null;
+            }
+            return (
+              <div
+                key={name}
+                className="flex flex-wrap items-baseline justify-start gap-6"
+              >
+                <legend className="whitespace-pre-wrap max-w-prose font-bold text-lead min-w-[4rem]">
+                  {name}
+                </legend>
+                <div className="flex flex-wrap items-baseline gap-4">
+                  <OptionRadio name={name} values={values} />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      }
+      <div>
+        <ProductPrice
+          className="text-gray-500 line-through text-lg font-semibold"
+          priceType="compareAt"
+          variantId={selectedVariant.id}
+          data={product}
+        />
+        <ProductPrice
+          className="text-gray-900 text-lg font-semibold"
+          variantId={selectedVariant.id}
+          data={product}
+        />
+      </div>
+      <div className="grid items-stretch gap-4">
+        {isOutOfStock ? (
+          <span className="text-black text-center py-3 px-6 border rounded-sm leading-none ">
+            Available in 2-3 weeks
+          </span>
+        ) : (
+          <BuyNowButton variantId={selectedVariant.id}>
+            <span className="bg-black text-white inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none w-full border">
+              Buy it now
+            </span>
+          </BuyNowButton>
+        )}
+      </div>
+    </form>
+  );
+}
 
-//   const isOutOfStock = !selectedVariant?.availableForSale || false;
-//   return (
-//     <form className="grid gap-10">
-//       {
-//         <div className="grid gap-4">
-//           {options.map(({ name, values }) => {
-//             if (values.length === 1) {
-//               return null;
-//             }
-//             return (
-//               <div
-//                 key={name}
-//                 className="flex flex-wrap items-baseline justify-start gap-6"
-//               >
-//                 <legend className="whitespace-pre-wrap max-w-prose font-bold text-lead min-w-[4rem]">
-//                   {name}
-//                 </legend>
-//                 <div className="flex flex-wrap items-baseline gap-4">
-//                   <OptionRadio name={name} values={values} />
-//                 </div>
-//               </div>
-//             );
-//           })}
-//         </div>
-//       }
-//       <div>
-//         <ProductPrice
-//           className="text-gray-500 line-through text-lg font-semibold"
-//           priceType="compareAt"
-//           variantId={selectedVariant.id}
-//           data={product}
-//         />
-//         <ProductPrice
-//           className="text-gray-900 text-lg font-semibold"
-//           variantId={selectedVariant.id}
-//           data={product}
-//         />
-//       </div>
-//       <div className="grid items-stretch gap-4">
-//         {isOutOfStock ? (
-//           <span className="text-black text-center py-3 px-6 border rounded-sm leading-none ">
-//             Available in 2-3 weeks
-//           </span>
-//         ) : (
-//           <BuyNowButton variantId={selectedVariant.id}>
-//             <span className="bg-black text-white inline-block rounded-sm font-medium text-center py-3 px-6 max-w-xl leading-none w-full border">
-//               Buy it now
-//             </span>
-//           </BuyNowButton>
-//         )}
-//       </div>
-//     </form>
-//   );
-// }
+function OptionRadio({ values, name }) {
+  const { selectedOptions, setSelectedOption } = useProductOptions();
+  // console.log("---",selectedOptions);
+  return (
+    <>
+      {values.map((value) => {
+        const checked = selectedOptions[name] === value;
+        const id = `option-${name}-${value}`;
 
-// function OptionRadio({ values, name }) {
-//   const { selectedOptions, setSelectedOption } = useProductOptions();
+        return (
+          <label key={id} htmlFor={id}>
+            <input
+              className="sr-only"
+              type="radio"
+              id={id}
+              name={`option[${name}]`}
+              value={value}
+              checked={checked}
+              onChange={() => setSelectedOption(name, value)}
+            />
+            <div
+              className={`leading-none border-b-[2px] py-1 cursor-pointer transition-all duration-200 ${
+                checked ? "border-gray-500" : "border-neutral-50"
+              }`}
+            >
+              {value}
+            </div>
+          </label>
+        );
+      })}
+    </>
+  );
+}
 
-//   return (
-//     <>
-//       {values.map((value) => {
-//         const checked = selectedOptions[name] === value;
-//         const id = `option-${name}-${value}`;
+function ProductGallery({ media }) {
+  if (!media.length) {
+    return null;
+  }
 
-//         return (
-//           <label key={id} htmlFor={id}>
-//             <input
-//               className="sr-only"
-//               type="radio"
-//               id={id}
-//               name={`option[${name}]`}
-//               value={value}
-//               checked={checked}
-//               onChange={() => setSelectedOption(name, value)}
-//             />
-//             <div
-//               className={`leading-none border-b-[2px] py-1 cursor-pointer transition-all duration-200 ${
-//                 checked ? "border-gray-500" : "border-neutral-50"
-//               }`}
-//             >
-//               {value}
-//             </div>
-//           </label>
-//         );
-//       })}
-//     </>
-//   );
-// }
+  return (
+    <div
+      className={`grid gap-4 overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}
+    >
+      {media.map((med, i) => {
+        let extraProps = {};
 
-// function ProductGallery({ media }) {
-//   if (!media.length) {
-//     return null;
-//   }
+        if (med.mediaContentType === "MODEL_3D") {
+          extraProps = {
+            interactionPromptThreshold: "0",
+            ar: true,
+            loading: "eager",
+            disableZoom: true,
+          };
+        }
 
-//   return (
-//     <div
-//       className={`grid gap-4 overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}
-//     >
-//       {media.map((med, i) => {
-//         let extraProps = {};
+        const data = {
+          ...med,
+          image: {
+            ...med.image,
+            altText: med.alt || "Product image",
+          },
+        };
 
-//         if (med.mediaContentType === "MODEL_3D") {
-//           extraProps = {
-//             interactionPromptThreshold: "0",
-//             ar: true,
-//             loading: "eager",
-//             disableZoom: true,
-//           };
-//         }
-
-//         const data = {
-//           ...med,
-//           image: {
-//             ...med.image,
-//             altText: med.alt || "Product image",
-//           },
-//         };
-
-//         return (
-//           <div
-//             className={`${
-//               i % 3 === 0 ? "md:col-span-2" : "md:col-span-1"
-//             } snap-center card-image bg-white aspect-square md:w-full w-[80vw] shadow-sm rounded`}
-//             key={med.id || med.image.id}
-//           >
-//             <MediaFile
-//               tabIndex="0"
-//               className={`w-full h-full aspect-square object-cover`}
-//               data={data}
-//               options={{
-//                 crop: "center",
-//               }}
-//               {...extraProps}
-//             />
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// }
+        return (
+          <div
+            className={`${
+              i % 3 === 0 ? "md:col-span-2" : "md:col-span-1"
+            } snap-center card-image bg-white aspect-square md:w-full w-[80vw] shadow-sm rounded`}
+            key={med.id || med.image.id}
+          >
+            <MediaFile
+              tabIndex="0"
+              className={`w-full h-full aspect-square object-cover`}
+              data={data}
+              options={{
+                crop: "center",
+              }}
+              {...extraProps}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
